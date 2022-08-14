4dc548151f025ed3abe1b2bd0361e02f068094fa
arthur007110 <arthurcosta007110@gmail.com>
2022-08-14 18:36:54 -0300

    adicionando validacao dos campos de endereco

--- app/models/endereco.rb
+++ app/models/endereco.rb
@@ -1,3 +1,9 @@
 class Endereco < ApplicationRecord
   belongs_to :paciente
+
+  validates :cep, presence: true, length: { is: 8 }, numericality: { only_integer: true }
+  validates :cidade, presence: true, length: { minimum: 3, maximum: 30 }, format: { with: /[a-zA-Z]+/, message: "only allows letters" }
+  validates :bairro, presence: true, length: { minimum: 3, maximum: 35 }, format: { with: /[a-zA-Z]+/, message: "only allows letters" }
+  validates :logradouro, presence: true, length: { minimum: 3, maximum: 60 }, format: { with: /[a-zA-Z]+/, message: "only allows letters" }
+  validates :complemento, presence: true, length: { minimum: 5, maximum: 35 }, format: { without: /!|@|#|\$|%|&/, message: "(! @ # $ % &) characters are not allowed" }
 end
