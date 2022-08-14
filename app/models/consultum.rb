9ba25778425d23483018454e2d1b51d33d9c3524
arthur007110 <arthurcosta007110@gmail.com>
2022-08-14 18:49:39 -0300

    adicionando validacao dos campos de consulta

--- app/models/consultum.rb
+++ app/models/consultum.rb
@@ -1,4 +1,7 @@
 class Consultum < ApplicationRecord
   has_one :paciente
   has_one :medico
+
+  validates :data, presence: true, in_future: true
+  validates :horario, presence: true, length: { is: 5 }, format: { with: /[\d]{2}:[\d]{2}/, message: "must be in 00:00 format" }
 end
