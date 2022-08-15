7db5245b935c479e2b6bc9d127a066bb052752da
arthur007110 <arthurcosta007110@gmail.com>
2022-08-14 21:12:00 -0300

    criando tela inicial

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
   resources :consulta
   resources :medicos
   resources :pacientes
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "welcome#index"
 end
