3247a8237561cf5007d4ea544ed65ef37a11283b
arthur007110 <arthurcosta007110@gmail.com>
2022-08-28 16:48:44 -0300

    testes para model paciente

--- test/models/paciente_test.rb
+++ test/models/paciente_test.rb
@@ -1,7 +1,25 @@
 require "test_helper"
 
 class PacienteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'Criar paciente valido' do
+    paciente = Paciente.new(
+      nome: 'Arthur Costa',
+      nascimento: '2000-01-18',
+      cpf: '12345678912',
+      email: 'a@a.com'
+    )
+
+    assert paciente.save
+  end
+
+  test 'Criar paciente com cpf invalido' do
+    paciente = Paciente.new(
+      nome: 'Arthur Costa',
+      nascimento: '2000-01-18',
+      cpf: '1234567891211',
+      email: 'a@a.com'
+    )
+
+    assert_not paciente.save
+  end
 end
